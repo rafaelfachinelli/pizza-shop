eda053ddc72b57af998fdb2c341e5b3f118aa341
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-01-26 12:42:52 -0300

    feat: Add sign-in functionality

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -21,15 +23,19 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
     try {
+      await authenticate({ email: data.email })
       toast.success('Enviamos um link de autenticação para o seu e-mail.', {
         action: {
           label: 'Reenviar e-mail',
           onClick: () => handleSignIn(data),
         },
       })
-      await new Promise((resolve) => setTimeout(resolve, 2000))
     } catch (error) {
       console.error('An error occurred while trying to sign in:', error)
       toast.error('Credenciais inválidas.')
