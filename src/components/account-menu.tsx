6e95b2165907fa3ad52d9bffa57171ab49f4ec47
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-01-26 13:08:01 -0300

    feat: Add API endpoints for getting managed restaurant and user profile

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,5 +1,9 @@
 import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu'
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
 
 import { Button } from './ui/button'
 import {
@@ -11,6 +15,16 @@ import {
 } from './ui/dropdown-menu'
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,15 +32,15 @@ export function AccountMenu() {
           variant="outline"
           className="flex items-center gap-2 select-none"
         >
-          Pizza Shop
+          {managedRestaurant?.name ?? 'Carregando'}
           <ChevronDown className="w-4 h-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Rafael Fachinelli</span>
+          <span>{profile?.name ?? 'Carregando'}</span>
           <span className="text-xs font-normal text-muted-foreground">
-            rafael.fachinelli@hotmail.com
+            {profile?.email ?? 'Carregando'}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
