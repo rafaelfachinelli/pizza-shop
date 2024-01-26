7c9a7e1fb4b73d2b8604ebb0b55605bb316b9461
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-01-25 21:50:50 -0300

    feat: create orders page table and filters layout

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,41 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        2g3253h36h6h3h36367
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Rafael Fachinelli</TableCell>
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="h-3 w-3 mr-2" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="h-3 w-3 mr-2" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -12,6 +12,9 @@ import {
   TableRow,
 } from '@/components/ui/table'
 
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
+
 export function Orders() {
   return (
     <>
@@ -19,12 +22,8 @@ export function Orders() {
       <div className="flex flex-col pad-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       </div>
+      <OrderTableFilters />
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
-
         <div className="border rounded-md">
           <Table>
             <TableHeader>
@@ -41,44 +40,7 @@ export function Orders() {
             </TableHeader>
             <TableBody>
               {Array.from({ length: 10 }).map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell>
-                    <Button variant="outline" size="xs">
-                      <Search className="h-3 w-3" />
-                      <span className="sr-only">Detalhes do pedido</span>
-                    </Button>
-                  </TableCell>
-                  <TableCell className="font-mono text-xs font-medium">
-                    2g3253h36h6h3h36367
-                  </TableCell>
-                  <TableCell className="text-muted-foreground">
-                    há 15 minutos
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex items-center gap-2">
-                      <span className="h-2 w-2 rounded-full bg-slate-400" />
-                      <span className="font-medium text-muted-foreground">
-                        Pendente
-                      </span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="font-medium">
-                    Rafael Fachinelli
-                  </TableCell>
-                  <TableCell className="font-medium">R$ 149,90</TableCell>
-                  <TableCell>
-                    <Button variant="outline" size="xs">
-                      <ArrowRight className="h-3 w-3 mr-2" />
-                      Aprovar
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button variant="ghost" size="xs">
-                      <X className="h-3 w-3 mr-2" />
-                      Cancelar
-                    </Button>
-                  </TableCell>
-                </TableRow>
+                <OrderTableRow key={index} />
               ))}
             </TableBody>
           </Table>
